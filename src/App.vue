9512646639ad7bb9d4a1596ed8bbab87a926f95b
Fucloud-arch <270928474@qq.com>
2023-12-13 19:23:57 +0800

    feat: init basic dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TalkRoom />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TalkRoom from './components/TalkRoom.vue'
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+    name: 'App',
+    components: {
+        TalkRoom
+    }
+}
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    padding: 0;
+    margin: 0;
+
+    width: 100%;
+    height: 100%;
 }
 </style>
---
+++ src/components/TalkRoom.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-container>
+        <el-header id="header">
+            <a>hello</a>
+        </el-header>
+
+        <el-footer></el-footer>
+    </el-container>
+</template>
+
+<script>
+export default {
+    name: 'TalkRoom'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#header {
+    background-color: black;
+    padding: 0;
+    margin: 0;
+}
+</style>
