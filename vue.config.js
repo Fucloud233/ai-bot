ca383baafb77b6a599f44858467432de9fbb2efc
Fucloud-arch <270928474@qq.com>
2023-12-14 15:27:56 +0800

    config: modify the listening port

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
-        port: 8080,
+        port: 6060,
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8081/',
+                target: 'http://127.0.0.1:6061/',
                 changeOrigin: true,
                 ws: false,
                 pathRewrite: {
