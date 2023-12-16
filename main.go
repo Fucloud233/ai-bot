2882be9ec4b2500416da19f6856f45401233b0f6
Warmguy <wangj865@mail2.sysu.edu.cn>
2023-12-16 12:26:42 +0800

    port modified

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"example.com/m/v2/router"
+	"example.com/m/v2/utils"
 )
 
 func main() {
+	utils.InitConfig()
+	utils.InitMySQL()
 	r := router.Router()
 
-	r.Run()
+	r.Run(":6062")
 }
