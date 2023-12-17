cd5e69427f6d0f7fe3eea07df45706715eee66aa
Fucloud-arch <270928474@qq.com>
2023-12-17 15:48:56 +0800

    feat: impl getNewestMessages api

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
     devServer: {
         port: 6060,
         proxy: {
+            '/db/api': {
+                target: 'http://127.0.0.1:6062/',
+                changeOrigin: true,
+                ws: false,
+                pathRewrite: {
+                    '^/db/api': ''
+                }
+            },
             '/api': {
                 target: 'http://127.0.0.1:6061/',
                 changeOrigin: true,
