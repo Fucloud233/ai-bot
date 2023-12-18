7e3a2b944c9fe8df3a83160e700f9809686d3cab
Fucloud-arch <270928474@qq.com>
2023-12-18 16:41:12 +0800

    refactor: error type

--- utils/error/error.go
+++ utils/error/error.go
@@ -1,8 +1,23 @@
 package error
 
-
 const (
-	JSONParseError string = "json can't be parsed"
-	UserNotFound string = "user not found" 
-	ParamLose string = "param lose"
+	JSONParseError int = 1001
+	ParamLose int = 1002
+
+	UserNotFound int = 2001
+	UserExist int = 2002
 )
+
+func GetErrorMessage(code int) interface{} {
+	var msg string
+	switch code {
+	case JSONParseError: msg = "json parse error"
+	case ParamLose: msg = "param lose"
+	case UserExist: msg = "user exits"
+	}
+
+	return map[string]interface{}{
+		"message": msg,
+		"code": code,
+	}
+}
