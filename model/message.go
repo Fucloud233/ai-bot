2e3583e901b038d3c6e3b789493283a8e57e3bc8
Fucloud-arch <270928474@qq.com>
2023-12-19 19:58:26 +0800

    feat: impl SimpleMessage for calling api

--- model/message.go
+++ model/message.go
@@ -2,12 +2,42 @@ package model
 
 import "time"
 
+const (
+	User string = "user"
+	Assistant string = "assistant"
+)
+
 type Message struct {
 	Phone string `json:"-" `
-	Role string `json:"role"`
 	BotRole string `json:"-"`
 	Time time.Time `json:"time"`
+	SimpleMessage
+}
+
+type SimpleMessage struct {
+	Role string `json:"role"`
 	Content string `json:"content"`
+}
+
+func NewMessage(phone string, botRole string, role string, content string, time time.Time) Message {
+	var message Message
+	message.Phone = phone
+	message.BotRole = botRole
+	message.Role = role
+	message.Content = content
+	message.Time = time
+	return message
+}
+
+func NewSimpleMessage(role string, content string) SimpleMessage {
+	return SimpleMessage {
+		Role: role,
+		Content: content,
+	}
+}
+
+func (msg *Message) ToSimpleMessage() SimpleMessage {
+	return NewSimpleMessage(msg.Role, msg.Content)
 }
 
 func AddMessages(messages *[]Message) error {
@@ -18,8 +48,8 @@ func GetNewestMessage(phone string, number int, size int) ([]Message, error) {
 	var message []Message
 	err := DB.Order("time").Limit(number).Offset(size).Where("phone = ?", phone).Find(&message).Error
 	// reserve array
-	for i, j := 0, len(message)-1; i < j; i, j = i+1, j-1 {
-		message[i], message[j] = message[j], message[i]
-	}
+	// for i, j := 0, len(message)-1; i < j; i, j = i+1, j-1 {
+	// 	message[i], message[j] = message[j], message[i]
+	// }
 	return message, err
 } 
