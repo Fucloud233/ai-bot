066cb4b8a4986d13343efdc84911f24a7c850b7c
Fucloud-arch <270928474@qq.com>
2023-12-20 15:03:11 +0800

    feat: impl MainLayout

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,82 @@
+<template>
+    <el-container id="container">
+        <el-header id="header">
+            <div class="tool-container" style="margin-left: 10px">
+                <slot name="leftTool"> </slot>
+            </div>
+            <h1 id="title">{{ title }}</h1>
+
+            <div class="tool-container" style="margin-right: 10px">
+                <slot name="rightTool"> </slot>
+            </div>
+        </el-header>
+
+        <el-main><slot name="main"></slot> </el-main>
+
+        <el-footer><slot name="footer"></slot> </el-footer>
+    </el-container>
+</template>
+
+<script>
+export default {
+    name: 'MainLayout',
+    props: {
+        title: {
+            type: String,
+            default: 'Title'
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+}
+
+#header {
+    background-color: #409eff;
+
+    height: 10%;
+    padding: 0;
+    margin: 0;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
+#title {
+    font-size: 20px;
+    font-family: Arial, sans-serif;
+    color: white;
+}
+.el-main {
+    display: flex;
+    flex-direction: column;
+    /* padding: 0 20px 10px 20px; */
+    max-height: 90%;
+    width: 100%;
+}
+.tool-container {
+    min-width: 30px;
+    width: 30px;
+}
+
+.el-icon {
+    font-size: 24px;
+    color: white;
+}
+.el-footer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 10%;
+    padding: 10px;
+    background: #f6f6f6;
+}
+</style>
