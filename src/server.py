4fcd7e34140ed839eb545e263869e4bf143370ab
Fucloud-arch <270928474@qq.com>
2023-12-25 18:54:29 +0800

    feat: define basic api for vector database

--- src/server.py
+++ src/server.py
@@ -3,16 +3,20 @@ from flask import Flask, request, send_file, make_response
 from flask_cors import CORS
 
 from bot import Bot, BotRole
+from chroma import VectorDB
 from typing import List
 
+from utils.vector_db import format_messages
 
-
+# flask
 app = Flask("ai-bot")
 CORS(app, supports_credentials=True)
-
+# bot
 bot = Bot()
+# vector database - chromadb
+vector_db = VectorDB()
 
-def gen_return_msg(msg: str, status_code: int=200):
+def gen_return_msg(msg: str='', status_code: int=200):
     response = make_response(msg)
     response.status_code = status_code
     return response
@@ -54,7 +58,64 @@ def chat_with_role(role_name):
     except TypeError as e:
         print(e.with_traceback())
         return gen_return_msg(repr(e), 400)
-        
+    
+
+@app.route("/vectordb/init", methods=['POST'])
+def init_database():
+    phone = request.json['phone']
+    try:
+        vector_db.init(phone)
+        return gen_return_msg()
+    except ValueError:
+        return gen_return_msg('user has existed', 400)
+
+
+@app.route("/vectordb/messages", methods=['POST'])
+def add_messages():
+    body = request.json
+
+    try:
+        phone = body['phone']
+        contents, roles = format_messages(body['messages'])
+    except KeyError as e:
+        return gen_return_msg(repr(e), 400)
+
+    try:
+        vector_db.append_messages(phone, contents, roles)
+        return gen_return_msg()
+    except FileNotFoundError:
+        return gen_return_msg('user not found', 404)
+    except ValueError as e:
+        return gen_return_msg(repr(e), 400)
+
+
+@app.route("/vectordb/messages", methods=['GET'])
+def clear_messages():
+    phone = request.args.get('phone')
+    result = vector_db.get_messages(phone)
+    return gen_return_msg({
+        "messages": result['documents']
+    })  
+
+@app.route("/vectordb/messages/all", methods=['DELETE'])
+def get_messages():
+    try:
+        phone = request.json['phone']
+        vector_db.clear_messages(phone)
+        return gen_return_msg()
+    except KeyError as e:
+        return gen_return_msg(repr(e), 400)
+    except FileNotFoundError as e:
+        return gen_return_msg(repr(e), 404)
+    
+
+# @app.route("/chroma/messages/similar")
+# def 
+
+# @app.route("/chroma/messages/newest")
+# def 
+    
+
 
 if __name__ == '__main__':
     app.run(port=6061, host="0.0.0.0", debug=True)
