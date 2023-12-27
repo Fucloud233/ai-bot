60e490e7e0d4dbc0fbf093a646f0432566ca2954
Fucloud-arch <270928474@qq.com>
2023-12-27 11:11:17 +0800

    feat: introduce prompt_wrap function

---
+++ src/utils/prompt.py
@@ -0,0 +1,16 @@
+def wrap_prompt(prompt, answer: str="好的，我知道了。"):
+    # messages must have an odd number of elements
+    return [{
+        "role": "user",
+        "content": prompt
+    }, {
+        "role": "assistant",
+        "content": answer
+    }]
+
+
+def wrap_user_prompt(prompt):
+    return {
+        "role": "user",
+        "content": prompt
+    }
