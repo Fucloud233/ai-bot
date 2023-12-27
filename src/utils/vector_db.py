f57d4532f6cd301b2c041c8b007f20fe0613891e
Fucloud-arch <270928474@qq.com>
2023-12-27 11:56:51 +0800

    feat: impl to_messages

--- src/utils/vector_db.py
+++ src/utils/vector_db.py
@@ -1,5 +1,34 @@
+from chromadb import QueryResult
+
 def format_messages(messages):
     contents = [message['content'] for message in messages]
     roles = [message['role'] for message in messages]
 
     return contents, roles
+
+# def to_messages(documents, metadatas):
+#     messages = []
+#     for (document, metadata) in zip(documents, metadatas):
+#         messages.append({
+#             "role": metadata['role'],
+#             "content": document,
+#             "id": metadata['id'],
+#         })
+
+#     return messages
+
+def to_messages(result: QueryResult, need_id: bool=False):
+    messages = []
+    for (document, metadata) in zip(result['documents'], result['metadatas']):
+        messages.append({
+            "role": metadata['role'],
+            "content": document,
+            # "id": metadata['id'],
+        })
+
+    if need_id:
+        for (message, id) in zip(messages, result['ids']):
+            message['id'] = id
+
+    return messages
+    
