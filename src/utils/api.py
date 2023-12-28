7f2dc65dfc17e0545305bc87c830e559de0a027b
Fucloud-arch <270928474@qq.com>
2023-12-28 17:16:10 +0800

    refactor: move recv_info function to here

--- src/utils/api.py
+++ src/utils/api.py
@@ -1,4 +1,25 @@
-from flask import make_response
+from flask import make_response, request
+    
+def recv_info():
+    try:
+        phone = request.json['phone']
+        bot_role = request.json['botRole']
+    except:
+        raise KeyError("key info not found")
+
+    return {
+        "phone": phone, 
+        "botRole": bot_role
+    }
+
+def recv_info_from_param():
+    try:
+        return {
+            "phone": request.args.get('phone'),
+            "botRole": request.args.get('botRole')
+        }
+    except:
+        raise KeyError("key info not found")
 
 def wrap_response(msg: str='', status_code: int=200):
     response = make_response(msg)
@@ -6,7 +27,9 @@ def wrap_response(msg: str='', status_code: int=200):
     return response
 
 def wrap_error(err: Exception, status_code: int):
-    resp = make_response(err.args[0])
+    resp = make_response({
+        "message": err.args[0]
+    })
     resp.status_code = status_code
     return resp
 
