846a987476e201692a7e91fdd68a113afb29355e
Fucloud-arch <270928474@qq.com>
2023-12-28 18:17:14 +0800

    refactor: move BotRole to here

--- src/utils/prompt.py
+++ src/utils/prompt.py
@@ -1,7 +1,24 @@
+from enum import Enum
 
 # role
 User = 'user'
 Assistant = 'assistant'
+
+class BotRole(Enum):
+    Parent = 'parent'
+    Bestie = 'bestie'
+    Friend = 'friend'
+    Doctor = 'doctor'
+
+    def new(role_name): 
+        return BotRole(role_name)
+    
+    def get_label(self) -> str:
+        match self:
+            case BotRole.Parent: return "父母"
+            case BotRole.Bestie: return "闺蜜"
+            case BotRole.Friend: return "朋友"
+            case BotRole.Doctor: return "心理医生"
 
 def wrap_prompt(prompt, answer: str="好的，我知道了。"):
     # messages must have an odd number of elements
