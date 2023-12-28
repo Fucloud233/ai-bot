679837e398be5f2343096e3befc815169ad49c37
Fucloud-arch <270928474@qq.com>
2023-12-28 14:43:49 +0800

    feat: receive botRole param

--- src/api/vector_db.py
+++ src/api/vector_db.py
@@ -4,33 +4,39 @@ vector_db_api = Blueprint('vector_db_api', __name__)
 
 from chroma import VectorDB
 from utils.vector_db import format_messages
-from utils.api import wrap_response
+from utils.api import wrap_response, wrap_error
 
 # vector database - chromadb
 vector_db = VectorDB()
 
+def get_basic_info():
+    try:
+        phone = request.json['phone']
+        bot_role = request.json['botRole']
+    except:
+        raise KeyError("key info not found")
+
+    return phone, bot_role
+
 @vector_db_api.route("/vectordb/init", methods=['POST'])
 def init_database():
-    phone = request.json['phone']
     try:
-        vector_db.init(phone)
+        phone, bot_role = get_basic_info()
+        vector_db.init(phone, bot_role)
         return wrap_response()
-    except ValueError:
-        return wrap_response('user has existed', 400)
-
+    except (KeyError, ValueError) as e:
+        return wrap_error(e, 400)
 
 @vector_db_api.route("/vectordb/messages", methods=['POST'])
 def add_messages():
-    body = request.json
-
     try:
-        phone = body['phone']
-        contents, roles = format_messages(body['messages'])
+        phone, bot_role = get_basic_info()
+        contents, roles = format_messages(request.json['messages'])
     except KeyError as e:
         return wrap_response(repr(e), 400)
 
     try:
-        vector_db.append_messages(phone, contents, roles)
+        vector_db.append_messages(phone, bot_role, contents, roles)
         return wrap_response()
     except FileNotFoundError:
         return wrap_response('user not found', 404)
@@ -40,17 +46,24 @@ def add_messages():
 
 @vector_db_api.route("/vectordb/messages", methods=['GET'])
 def get_messages():
-    phone = request.args.get('phone')
-    messages = vector_db.get_messages(phone)
-    return wrap_response({
-        "messages": messages
-    })  
+    try:
+        # Notice: Get Request here
+        phone = request.args.get('phone')
+        bot_role = request.args.get('botRole')
+        messages = vector_db.get_messages(phone, bot_role)
+        return wrap_response({
+            "messages": messages
+        })  
+    except KeyError as e:
+        return wrap_response(repr(e), 400)
+    except FileNotFoundError:
+        return wrap_response("User or BotRole not found", 404)
 
 @vector_db_api.route("/vectordb/messages/all", methods=['DELETE'])
 def clear_messages():
     try:
-        phone = request.json['phone']
-        vector_db.clear_messages(phone)
+        phone, bot_role = get_basic_info()
+        vector_db.clear_messages(phone, bot_role)
         return wrap_response()
     except KeyError as e:
         return wrap_response(repr(e), 400)
--- src/chroma.py
+++ src/chroma.py
@@ -6,38 +6,44 @@ from chromadb.utils import embedding_functions
 
 from utils.error import UserNotFound, UserExist
 from utils.vector_db import to_messages
+
+
+def merge_name(phone: str, bot_role):
+    return phone + '-' + bot_role
     
 class VectorDB:
     def __init__(self, path: str='chroma/'):
         self.client = chromadb.PersistentClient(path=path)
         self.embedding_function = embedding_functions \
             .SentenceTransformerEmbeddingFunction(model_name='gtr-t5-base')
-
-    def init(self, name: str):
+        
+    def init(self, phone: str, bot_role: str):
         try:
-            self.client.create_collection(name=name)
+            self.client.create_collection(name=merge_name(phone, bot_role))
         except:
-            raise UserExist
+            raise ValueError('botRole has existed')
 
-    def __get_collection(self, phone: str):
+    def __get_collection(self, phone: str, bot_role: str):
         try:
-            return self.client.get_collection(phone, embedding_function=self.embedding_function) 
+            return self.client.get_collection(
+                merge_name(phone, bot_role), 
+                embedding_function=self.embedding_function
+            ) 
         except ValueError:
             raise UserNotFound
 
-    # TODO: set roleType when append messages
-    def append_messages(self, phone: str, contents: List[str], roles: List[str]):
+    def append_messages(self, phone: str, bot_role: str, contents: List[str], roles: List[str]):
         if len(contents) != len(roles):
             raise ValueError('the length of contents is not equal with the roles')
 
-        collection = self.__get_collection(phone)
+        collection = self.__get_collection(phone, bot_role)
         start_id = collection.count()
         
         # generate id for those content
         ids = [start_id + i for i in range(0, len(contents)) ]        
 
         print(roles, ids)
-        self.__get_collection(phone) \
+        self.__get_collection(phone, bot_role) \
             .add(
                 ids=[str(id) for id in ids], 
                 documents=contents, 
@@ -47,17 +53,17 @@ class VectorDB:
                 } for role, id in zip(roles, ids)]
             )
     
-    def clear_messages(self, phone: str):
+    def clear_messages(self, phone: str, bot_role: str):
         self.client.delete_collection(phone)
         self.client.create_collection(phone)
 
-    def get_messages(self, phone: str):
-        result = self.__get_collection(phone).get()
+    def get_messages(self, phone: str, bot_role: str):
+        result = self.__get_collection(phone, bot_role).get()
         return to_messages(result, need_id=True)
     
     # TODO: check duplicated message between query messages and history messages
-    def query(self, phone: str, message: str):
-        collection = self.__get_collection(phone)
+    def query(self, phone: str, bot_role: str, message: str):
+        collection = self.__get_collection(phone, bot_role)
         result = collection.query(query_texts=message, n_results=1)
 
         messages = to_messages(result, is_multiple=True)
