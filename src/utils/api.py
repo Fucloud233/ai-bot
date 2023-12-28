d443c1c983c4e8a18f5bd7a22a893fc63f4975aa
Fucloud-arch <270928474@qq.com>
2023-12-28 14:42:04 +0800

    feat: impl wrap_error function

--- src/utils/api.py
+++ src/utils/api.py
@@ -5,6 +5,11 @@ def wrap_response(msg: str='', status_code: int=200):
     response.status_code = status_code
     return response
 
+def wrap_error(err: Exception, status_code: int):
+    resp = make_response(err.args[0])
+    resp.status_code = status_code
+    return resp
+
 def check_messages(messages):
     if type(messages) != list:
         raise TypeError("The format of messages is incorrect!")
