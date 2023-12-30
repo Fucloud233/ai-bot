77c625919436807b8102a09f831569eaedbf5825
Fucloud-arch <270928474@qq.com>
2023-12-31 00:13:03 +0800

    feat: impl get_nearest_messages

--- src/api/vector_db.py
+++ src/api/vector_db.py
@@ -4,7 +4,7 @@ vector_db_api = Blueprint('vector_db_api', __name__)
 
 from chroma import VectorDB
 from utils.vector_db import format_messages
-from utils.api import wrap_response, wrap_error, recv_info, recv_info_from_param
+from utils.api import wrap_response, wrap_error
 import utils.api as apiUtils
 
 # vector database - chromadb
@@ -13,7 +13,7 @@ vector_db = VectorDB()
 @vector_db_api.route("/vectordb/init", methods=['POST'])
 def init_database():
     try:
-        info = recv_info()
+        info = apiUtils.body_index()
         vector_db.init(info)
         return wrap_response()
     except (KeyError, ValueError) as e:
@@ -22,7 +22,7 @@ def init_database():
 @vector_db_api.route("/vectordb/messages", methods=['POST'])
 def add_messages():
     try:
-        info = recv_info()
+        info = apiUtils.body_index()
         contents, roles = format_messages(request.json['messages'])
         vector_db.append_messages(info, contents, roles)
         return wrap_response()
@@ -35,11 +35,11 @@ def add_messages():
 def get_messages():
     try:
         # Notice: Get Request here
-        info = recv_info_from_param()
+        info = apiUtils.param_index()
         
         # whether it will return with id
-        need_id = apiUtils.recv_bool_from_parm('needId')
-        need_time = apiUtils.recv_bool_from_parm('needTime')
+        need_id = apiUtils.param_bool('needId')
+        need_time = apiUtils.param_bool('needTime')
 
         messages = vector_db.get_messages(info, need_id, need_time)
         return wrap_response({
@@ -50,10 +50,30 @@ def get_messages():
     except FileNotFoundError as e:
         return wrap_error(e, 404)
 
+@vector_db_api.route("/vectordb/messages/nearest", methods=['GET'])
+def get_nearest_messages():
+    try:
+        messages = vector_db.get_nearest_messages(
+            apiUtils.param_index(),
+            apiUtils.param_int("number", 10),
+            apiUtils.param_int("offset", 0),
+            apiUtils.param_int("n", 10),
+            apiUtils.param_bool("needId"),
+            apiUtils.param_bool("needTime")
+        )
+        return wrap_response({
+            "messages": messages
+        })  
+    except KeyError as e:
+        return wrap_response(repr(e), 400)
+    except FileNotFoundError as e:
+        return wrap_error(e, 404)  
+
+
 @vector_db_api.route("/vectordb/messages/all", methods=['DELETE'])
 def clear_messages():
     try:
-        info = recv_info()
+        info = apiUtils.body_info()
         vector_db.clear_messages(info)
         return wrap_response()
     except KeyError as e:
---
+++ src/utils/time.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+
+MINUTE = 60 
+
+def get_now_timestamp():
+    return datetime.now().timestamp()
